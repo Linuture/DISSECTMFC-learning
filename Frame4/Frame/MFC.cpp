9c64ee32a2419b31728de4ab7701fbe8bc8b4944
Linuture <26655268+Linuture@users.noreply.github.com>
2018-09-03 11:17:34 +0800

    Add files via upload

---
+++ Frame4/Frame/MFC.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "MFC.h"
+#include "myHeader/myMFC.h"  // it should be mfc.h, but for CMyWinApp definition, so...
+
+extern CMyWinApp theApp;
+
+static char szCObject[] = "CObject";
+struct CRuntimeClass CObject::classCObject =
+{ szCObject, sizeof (CObject), 0xffff, nullptr, nullptr, nullptr };
+static AFX_CLASSINIT _init_CObject (&CObject::classCObject);
+
+CRuntimeClass * CRuntimeClass::firstClass = nullptr;
+
+CWinApp * AfxGetApp ()
+{
+	return theApp.m_currentWinApp;
+}
+
+IMPLEMENT_DYNAMIC (CCmdTarget, CObject)
+IMPLEMENT_DYNAMIC (CWinThread, CCmdTarget)
+IMPLEMENT_DYNAMIC (CWinApp, CWinThread)
+IMPLEMENT_DYNAMIC (CWnd, CCmdTarget)
+IMPLEMENT_DYNAMIC (CFrameWnd, CWnd)
+IMPLEMENT_DYNAMIC (CDocument, CCmdTarget)
+IMPLEMENT_DYNAMIC (CView, CWnd)
